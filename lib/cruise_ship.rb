ce836d2a1d9bd2d927ab3dc1269dcee56b6349aa
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-16 01:29:24 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,11 +11,12 @@ def select_winner(passengers)
   winner = ""
 
   passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
+    if suite == :suite_a && name.start_with?("A")
     winner = name
   end
-  array
+  
 end
+array
 
 
 end
